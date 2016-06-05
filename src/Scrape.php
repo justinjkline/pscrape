97d58a9b96eef9281790040e43fd5e3318d02323
Justin Kline <justin@markerly.com>
2016-06-05 15:15:47 -0500

    updated composer.json

--- src/Scrape.php
+++ src/Scrape.php
@@ -1,2 +1,15 @@
 <?php
 
+require __DIR__.'/../vendor/autoload.php';
+$rollingCurl = new \RollingCurl\RollingCurl();
+    
+class Scrape {
+
+    public function hello() {
+        echo "<pre>";
+        print_r($rollingCurl);
+    }
+
+}
+
+?>
