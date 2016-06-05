cf037b132909c6c497be977b92ae212b9e2e994f
Justin Kline <justin@markerly.com>
2016-06-05 12:41:31 -0500

    ugh

---
+++ PscrapeServiceProvider.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Pscrape\Pscrape;
+
+use Illuminate\Support\ServiceProvider;
+
+ 
+class PscrapeServiceProvider extends ServiceProvider {
+    
+    public function boot()
+    {
+        /*
+        |--------------------------------------------------------------------------
+        | Publish the Config file from the Package to the App directory
+        |--------------------------------------------------------------------------
+        */
+        $this->configPublisher();
+    }
+
+
+    private function configPublisher()
+    {
+        // When users execute Laravel's vendor:publish command, the config file will be copied to the specified location
+        $this->publishes([
+            __DIR__.'/Config/pscrape.php' => config_path('pscrape.php'),
+        ]);
+    }
